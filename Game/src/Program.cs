482086a5fe81d8fe0d8013989bb09dd036eb6b78
Unknown <cournalpwn@gmail.com>
2017-10-01 05:08:36 -0400

    Finished Setting Up Scene
    Need to fix collision bounds and Z level decisions

--- Game/src/Program.cs
+++ Game/src/Program.cs
@@ -31,7 +31,7 @@ namespace SpaceExplorers
             Camera defaultCamera = new Camera(defaultView);
 
             //Create the Room
-            ActiveRoom = new Room(1000, 1000, defaultCamera);
+            ActiveRoom = new Room(2000, 1000, defaultCamera);
             Controller.Set_Room(ActiveRoom);
 
             //Create the Hud
@@ -70,17 +70,56 @@ namespace SpaceExplorers
 
             //Create New Mapentitys and add them to the room
             ActiveRoom.EntityList.Add((RoomEntity)EntityLibrary.Create("Background"));
-            ActiveRoom.EntityList.Add((RoomEntity)EntityLibrary.Create("DebugBox", new Vector(300, 250)));
-            ActiveRoom.EntityList.Add((RoomEntity)EntityLibrary.Create("DebugBox", new Vector(500, 300)));
-            ActiveRoom.EntityList.Add((RoomEntity)EntityLibrary.Create("DebugBox", new Vector(450, 450)));
-            ActiveRoom.EntityList.Add((RoomEntity)EntityLibrary.Create("DebugBox", new Vector(100, 550)));
-            ActiveRoom.EntityList.Add((RoomEntity)EntityLibrary.Create("DebugCube", new Vector(150, 300)));
+            ActiveRoom.EntityList.Add((RoomEntity)EntityLibrary.Create("Rock1", new Vector(88, 509)));
+            ActiveRoom.EntityList.Add((RoomEntity)EntityLibrary.Create("Rock2", new Vector(61, 507)));
+            ActiveRoom.EntityList.Add((RoomEntity)EntityLibrary.Create("Rock3", new Vector(207, 587)));
+            ActiveRoom.EntityList.Add((RoomEntity)EntityLibrary.Create("Rock4", new Vector(370, 284)));
+            ActiveRoom.EntityList.Add((RoomEntity)EntityLibrary.Create("Rock5", new Vector(586, 208)));
+            ActiveRoom.EntityList.Add((RoomEntity)EntityLibrary.Create("Rock6", new Vector(633, 195)));
+            ActiveRoom.EntityList.Add((RoomEntity)EntityLibrary.Create("Rock7", new Vector(918, 393)));
+            ActiveRoom.EntityList.Add((RoomEntity)EntityLibrary.Create("Rock8", new Vector(869, 373)));
+            ActiveRoom.EntityList.Add((RoomEntity)EntityLibrary.Create("Rock9", new Vector(858, 601)));
+            ActiveRoom.EntityList.Add((RoomEntity)EntityLibrary.Create("Rock10", new Vector(812, 601)));
+            ActiveRoom.EntityList.Add((RoomEntity)EntityLibrary.Create("Rock11", new Vector(617, 680)));
+            ActiveRoom.EntityList.Add((RoomEntity)EntityLibrary.Create("Bush1", new Vector(31, 546)));
+            ActiveRoom.EntityList.Add((RoomEntity)EntityLibrary.Create("Bush1", new Vector(-14, 280)));
+            ActiveRoom.EntityList.Add((RoomEntity)EntityLibrary.Create("Bush1", new Vector(1497, 568)));
+            ActiveRoom.EntityList.Add((RoomEntity)EntityLibrary.Create("Bush2", new Vector(44, 392)));
+            ActiveRoom.EntityList.Add((RoomEntity)EntityLibrary.Create("Bush2", new Vector(148, 780)));
+            ActiveRoom.EntityList.Add((RoomEntity)EntityLibrary.Create("Bush2", new Vector(1696, 394)));
+            ActiveRoom.EntityList.Add((RoomEntity)EntityLibrary.Create("Bush3", new Vector(1497, 622)));
+            ActiveRoom.EntityList.Add((RoomEntity)EntityLibrary.Create("Bush4", new Vector(1585, 602)));
+            ActiveRoom.EntityList.Add((RoomEntity)EntityLibrary.Create("Tree1", new Vector(536, 523)));
+            ActiveRoom.EntityList.Add((RoomEntity)EntityLibrary.Create("Tree2", new Vector(79, 376)));
+            ActiveRoom.EntityList.Add((RoomEntity)EntityLibrary.Create("Tree3", new Vector(957, 172)));
+            ActiveRoom.EntityList.Add((RoomEntity)EntityLibrary.Create("TreesBack", new Vector(0, 0)));
+            ActiveRoom.EntityList.Add((RoomEntity)EntityLibrary.Create("TreesFore", new Vector(0, 400)));
+            ActiveRoom.EntityList.Add((RoomEntity)EntityLibrary.Create("BeaconLightRadar", new Vector(962, 235)));
+            ActiveRoom.EntityList.Add((RoomEntity)EntityLibrary.Create("DirtForeBeacon", new Vector(1027, 528)));
+            ActiveRoom.EntityList.Add((RoomEntity)EntityLibrary.Create("DirtForeBeacon2", new Vector(1234, 429)));
+            ActiveRoom.EntityList.Add((RoomEntity)EntityLibrary.Create("Garden", new Vector(544, 307)));
+            ActiveRoom.EntityList.Add((RoomEntity)EntityLibrary.Create("Plants", new Vector(546, 295)));
+            ActiveRoom.EntityList.Add((RoomEntity)EntityLibrary.Create("Shelter1", new Vector(1178, 554)));
+            ActiveRoom.EntityList.Add((RoomEntity)EntityLibrary.Create("Shelter2", new Vector(779, 644)));
+            ActiveRoom.EntityList.Add((RoomEntity)EntityLibrary.Create("Shelter3", new Vector(855, 390)));
+            ActiveRoom.EntityList.Add((RoomEntity)EntityLibrary.Create("Shelter4", new Vector(690, 506)));
+            ActiveRoom.EntityList.Add((RoomEntity)EntityLibrary.Create("Shelter5", new Vector(486, 600)));
+            ActiveRoom.EntityList.Add((RoomEntity)EntityLibrary.Create("Shelter6", new Vector(483, 746)));
+            ActiveRoom.EntityList.Add((RoomEntity)EntityLibrary.Create("Shelter7", new Vector(281, 694)));
+            ActiveRoom.EntityList.Add((RoomEntity)EntityLibrary.Create("Shelter8", new Vector(187, 251)));
+            ActiveRoom.EntityList.Add((RoomEntity)EntityLibrary.Create("Sawmill", new Vector(1323, 442)));
+            ActiveRoom.EntityList.Add((RoomEntity)EntityLibrary.Create("ClothingLine", new Vector(1041, 123)));
+            ActiveRoom.EntityList.Add((RoomEntity)EntityLibrary.Create("CargoMod", new Vector(751, 60)));
+            ActiveRoom.EntityList.Add((RoomEntity)EntityLibrary.Create("CryoMod", new Vector(503, 66)));
+            ActiveRoom.EntityList.Add((RoomEntity)EntityLibrary.Create("MedMod", new Vector(1476, 302)));
+            ActiveRoom.EntityList.Add((RoomEntity)EntityLibrary.Create("NuclearMod", new Vector(303, 315)));
+            ActiveRoom.EntityList.Add((RoomEntity)EntityLibrary.Create("NuclearBeaconWire", new Vector(448, 494)));
             ActiveRoom.EntityList.Add((Actor)EntityLibrary.Create("Jacob", new Vector(370, 400)));
-            Actor AHavisham = (Actor)EntityLibrary.Create("AHavisham", new Vector(100, 200));
+            Actor AHavisham = (Actor)EntityLibrary.Create("AHavisham", new Vector(100, 350));
             ActiveRoom.EntityList.Add(AHavisham);
             Controller.Set_Control(AHavisham);
             defaultCamera.Set_Focus(AHavisham);
-            Actor JBreech = (Actor)EntityLibrary.Create("JBreech", new Vector(150, 200));
+            Actor JBreech = (Actor)EntityLibrary.Create("JBreech", new Vector(150, 350));
             Action<Entity, Entity> dialogue = (char1, char2) => DialogueEngine.RunDialogue((Actor)char1, (Actor)char2);
             JBreech.InitializeInteraction(new Vector(26, 47), dialogue);
             ActiveRoom.EntityList.Add(JBreech);
